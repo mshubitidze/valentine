41d5f98aaef9889d49065d6edc9ff622be737d3f
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2024-02-03 13:48:07 +0400

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media1.tenor.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.tenor.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
