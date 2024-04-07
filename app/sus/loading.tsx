41a1f9590ab07b91f61d26eeb385f10300ea748d
mshubitidze <59510085+mshubitidze@users.noreply.github.com>
2024-04-07 22:08:45 +0400

    dev

---
+++ app/sus/loading.tsx
@@ -0,0 +1,7 @@
+export default function SusLoading() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="animate-spin text-3xl">Loading..</div>
+    </main>
+  );
+}
---
+++ app/sus/page.tsx
@@ -0,0 +1,6 @@
+export default async function SusPage() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  return (
+    <main className="flex items-center justify-center min-h-screen">sus</main>
+  );
+}
